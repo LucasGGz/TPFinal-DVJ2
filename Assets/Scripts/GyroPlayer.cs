d718ab2d9533c6eebe3eeae4127466d0d2eb2343
LucasGGz <112144033+LucasGGz@users.noreply.github.com>
2023-11-23 13:33:25 -0300

    Initial commit

---
+++ Assets/Scripts/GyroPlayer.cs
@@ -0,0 +1,31 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class GyroPlayer : MonoBehaviour
+{
+    Gyroscope steerGyro;
+    public Rigidbody capsule;
+    public float speed;
+
+    void Start()
+    {
+        // Habilita el giroscopio
+        steerGyro = Input.gyro;
+        steerGyro.enabled = true;
+    }
+
+    void Update()
+    {
+        // Transforma la rotación basada en el giroscopio
+        //Quaternion gyroRotation = steerGyro.attitude;
+       // gyroRotation = Quaternion.Euler(90f, 0f, 0f) * gyroRotation;
+
+        // Calcula la velocidad basada en la inclinación del giroscopio en el eje X y Z
+        float speedX = steerGyro.rotationRate.x * speed;
+        float speedZ = steerGyro.rotationRate.z * speed;
+
+        // Aplica la velocidad al rigidbody
+        capsule.velocity = new Vector3(0f, 0f, speedZ);
+    }
+}
