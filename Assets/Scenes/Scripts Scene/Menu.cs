4e8c7e4042d56fca19cd9add5b22ae1356de61cb
Alexis Pacheco <nuve98@gmail.com>
2023-11-24 14:30:54 -0300

    alexis

---
+++ Assets/Scenes/Scripts Scene/Menu.cs
@@ -0,0 +1,22 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class Menu : MonoBehaviour
+{
+    public Animator animFade;
+
+    public void Scene01()
+    {
+        animFade.SetBool("Oculto", true);
+        StartCoroutine(Load());
+
+        IEnumerator Load()
+        {
+            yield return new WaitForSeconds(3.0f);
+            //SceneManager.LoadScene(1);
+            SceneManager.LoadScene("TerrainScene");
+        }
+    }
+}
